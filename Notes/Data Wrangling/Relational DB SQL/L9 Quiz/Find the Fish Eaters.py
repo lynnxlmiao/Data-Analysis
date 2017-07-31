5f3c636b8412112b388593431f6818dc19ef6496
lynnxlmiao <mxl925202@outlook.com>
2017-07-31 23:31:59 +0800

    Find the FIsh Eaters

---
+++ Notes/Data Wrangling/Relational DB SQL/L9 Quiz/Find the Fish Eaters.py
@@ -0,0 +1,20 @@
+#
+# Find the names of the individual animals that eat fish.
+#
+# The animals table has columns (name, species, birthdate) for each individual.
+# The diet table has columns (species, food) for each food that a species eats.
+#
+
+QUERY = '''
+SELECT animals.name
+  FROM animals JOIN diet
+    ON animals.species = diet.species
+ WHERE diet.food = 'fish'
+'''
+
+QUERY_OPTIONAL = '''
+SELECT name
+  FROM animals,diet
+ WHERE animals.species = diet.species
+ and diet.food = 'fish'
+'''
