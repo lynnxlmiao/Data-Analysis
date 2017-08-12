27d9a75cdf0dfce5db690133c9ddec5150d052ba
lynnxlmiao <mxl925202@outlook.com>
2017-08-12 15:25:38 +0800

    iterative parsing

---
+++ Projects/OpenStreetMap Data Case Study/Iterative_pasring.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import xml.etree.cElementTree as ET
+import pprint
+
+OSM_PATH = "C:\Files\Udacity\Data Analyst Nanodegree\Part 4\Singapore\Singapore.osm"
+
+
+def count_tags(filename):
+    result = dict()
+
+    for a, b in ET.iterparse(filename, events=('start',)):
+        result[b.tag] = result.get(b.tag, 0) + 1
+    return result
+
+
+def test():
+    tags = count_tags(OSM_PATH)
+    print
+    pprint.pprint(tags)
+    print
+
+
+if __name__ == "__main__":
+    test()
